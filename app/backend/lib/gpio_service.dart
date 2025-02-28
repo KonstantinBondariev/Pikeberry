c4e68405ad6ec9316a8ad3be7494d24743447c1b
Konstantin Bondariev <bondariev.konstantin@gmail.com>
2025-02-28 22:53:44 +0200

    Enhance GPIO test function with default configuration and additional GPIO instances

--- app/backend/lib/gpio_service.dart
+++ app/backend/lib/gpio_service.dart
@@ -25,30 +25,36 @@ class GpioService {
   }
 
 void test() {
-  var config = GPIOconfig(
-    GPIOdirection.gpioDirOut, 
-    GPIOedge.none, 
-    GPIObias.defaultBias, 
-    GPIODrive.defaultDrive, 
-    false, 
-    'test_gpio'
-  );
+  var config = GPIOconfig.defaultValues();
+  config.direction = GPIOdirection.gpioDirOut;
 
   print('Native c-periphery Version : ${getCperipheryVersion()}');
   print('GPIO test');
 
-  var gpio = GPIO.advanced(18, config); // Используем GPIO.advanced
+  var gpio = GPIO.advanced(18, config);
+  var gpio2 = GPIO(16, GPIOdirection.gpioDirOut);
+  var gpio3 = GPIO.advanced(5, config);
 
-  print('GPIO info: ' + gpio.getGPIOinfo());
+  print('GPIO info: ${gpio.getGPIOinfo()}');
+  print('GPIO native file handle: ${gpio.getGPIOfd()}');
+  print('GPIO chip name: ${gpio.getGPIOchipName()}');
+  print('GPIO chip label: ${gpio.getGPIOchipLabel()}');
 
   for (var i = 0; i < 10; ++i) {
     gpio.write(true);
+    gpio2.write(true);
+    gpio3.write(true);
     sleep(Duration(milliseconds: 200));
     gpio.write(false);
+    gpio2.write(false);
+    gpio3.write(false);
     sleep(Duration(milliseconds: 200));
   }
 
   gpio.dispose();
+  gpio2.dispose();
+  gpio3.dispose();
 }
+
 
 }
